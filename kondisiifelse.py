f3a683268a59d7fc25d234b26f683fe5040d3df7
Glenn Vhalado <34154595+glennvhalado@users.noreply.github.com>
2022-04-19 15:43:07 +0700

    Update kondisiifelse.py

--- kondisiifelse.py
+++ kondisiifelse.py
@@ -1,6 +1,6 @@
 #Kondisi if else adalah jika kondisi bernilai TRUE maka akan dieksekusi pada if, tetapi jika bernilai FALSE maka akan dieksekusi kode pada else
 
-nilai = 3
+nilai = 5
 #Jika pernyataan pada if bernilai TRUE maka if akan dieksekusi, tetapi jika FALSE kode pada else yang akan dieksekusi.
 if(nilai > 7):
     print("Selamat Anda Lulus")
